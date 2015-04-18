92b941ba3218f979e06a3d9331ae05d88195b727
Brendon Dugan <wishingforayer@gmail.com>
2015-04-17 22:56:44 -0400

    Implemented the SingleDigitConverter class so that it now passes the test defined earlier.

--- src/main/java/com/brendondugan/n2te/util/SingleDigitConverter.java
+++ src/main/java/com/brendondugan/n2te/util/SingleDigitConverter.java
@@ -5,11 +5,13 @@ package com.brendondugan.n2te.util;
  */
 public class SingleDigitConverter implements DigitConverter {
 
+    private String[] values = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
     @Override
     public String convertDigit(int digit) throws IllegalArgumentException{
         if(digit > 9 || digit < 0){
             throw new IllegalArgumentException("SingleDigitConverter is intended to work on numbers between 0 and 9");
         }
-        return null;
+        return values[digit];
     }
 }
