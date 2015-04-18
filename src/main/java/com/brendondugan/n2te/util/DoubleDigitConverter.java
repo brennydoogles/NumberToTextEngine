6451cbd27a0497855bb8523e1523c92dd8b4f8e9
Brendon Dugan <wishingforayer@gmail.com>
2015-04-17 23:17:30 -0400

    Added the DoubleDigitConverter class which will be used to convert double digit integers into text. Added a Unit Test which will show when the DoubleDigitConverter is working to specification.

---
+++ src/main/java/com/brendondugan/n2te/util/DoubleDigitConverter.java
@@ -0,0 +1,11 @@
+package com.brendondugan.n2te.util;
+
+/**
+ * Created by brendon on 4/17/2015.
+ */
+public class DoubleDigitConverter implements DigitConverter {
+    @Override
+    public String convertDigit(int digit) {
+        return null;
+    }
+}
---
+++ src/test/java/com/brendondugan/n2te/util/DoubleDigitConverterTest.java
@@ -0,0 +1,45 @@
+package com.brendondugan.n2te.util;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DoubleDigitConverterTest {
+
+    private DoubleDigitConverter converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new DoubleDigitConverter();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        converter = null;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConvertDigit() throws Exception {
+        assertEquals(converter.convertDigit(10), "ten");
+        assertEquals(converter.convertDigit(11), "eleven");
+        assertEquals(converter.convertDigit(12), "twelve");
+        assertEquals(converter.convertDigit(13), "thirteen");
+        assertEquals(converter.convertDigit(14), "fourteen");
+        assertEquals(converter.convertDigit(15), "fifteen");
+        assertEquals(converter.convertDigit(16), "sixteen");
+        assertEquals(converter.convertDigit(17), "seventeen");
+        assertEquals(converter.convertDigit(18), "eighteen");
+        assertEquals(converter.convertDigit(19), "nineteen");
+        assertEquals(converter.convertDigit(20), "twenty");
+        assertEquals(converter.convertDigit(31), "thirty one");
+        assertEquals(converter.convertDigit(42), "forty two");
+        assertEquals(converter.convertDigit(53), "fifty three");
+        assertEquals(converter.convertDigit(64), "sixty four");
+        assertEquals(converter.convertDigit(75), "seventy five");
+        assertEquals(converter.convertDigit(86), "eighty six");
+        assertEquals(converter.convertDigit(97), "ninety seven");
+        converter.convertDigit(100);
+    }
+}
