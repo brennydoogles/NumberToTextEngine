58d93d86d2e4b7c921a8745e189f111854601402
Brendon Dugan <wishingforayer@gmail.com>
2015-04-18 00:54:44 -0400

    Implemented the TripleDigitConverter class so that it now passes the test defined earlier.

--- src/main/java/com/brendondugan/n2te/util/TripleDigitConverter.java
+++ src/main/java/com/brendondugan/n2te/util/TripleDigitConverter.java
@@ -9,7 +9,15 @@ public class TripleDigitConverter implements DigitConverter {
         if(digit > 999 || digit < 0){
             throw new IllegalArgumentException("TripleDigitConverter is intended to work on numbers between 0 and 999");
         }
+        int hundredsPlace = digit / 100;
+        int remainder = digit % 100;
+        if(hundredsPlace > 0){
+            if(remainder > 0){
+                return String.format("%s hundred and %s", new SingleDigitConverter().convertDigit(hundredsPlace), new DoubleDigitConverter().convertDigit(remainder)).trim();
+            }
+            return String.format("%s hundred", new SingleDigitConverter().convertDigit(hundredsPlace)).trim();
 
-        return null;
+        }
+        return new DoubleDigitConverter().convertDigit(remainder).trim();
     }
 }
